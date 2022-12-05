7b74d4cd11e57d4420661e3400fd74ecee910ca1
Abdirizak Mohamed <abdirizakm123@gmail.com>
2022-12-05 21:09:19 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-  document.getElementById(".new-task-description");
+let form = document.querySelector("form");
 
-  function myFunction() {
-    document.getElementById("create-task-form").submit();
-    
-  }
+document.querySelector("form").addEventListener('submit',(e) => {
+  e.preventDefault();
+  let inputVal = document.getElementById("new-task-description").value;
+  const ul = document.getElementById("tasks");
+  const btn = document.createElement("button");
+  btn.textContent = "X";
+  btn.addEventListener("click",handleDelete);
+  const li = document.createElement("li");
+  li.textContent = ` ${inputVal} `
+  li.append(btn)
+  ul.append(li)
+  form.reset()
 
-  
-
+  })
 });
+
+function handleDelete(e) {
+  console.log(e.target.parentNode.remove())
+}
